711cd930d37da60d37d54f41c771ff4a0938d74f
Yoan Tournade <yoan.tournade@gmail.com>
2015-11-25 20:56:22 +0100

    update the SD card logger example

--- examples/SdCardCsvLogger/SdCardCsvLogger.ino
+++ examples/SdCardCsvLogger/SdCardCsvLogger.ino
@@ -2,35 +2,36 @@
 #include <SD.h>
 #include <SPI.h>
 
-const int chipSelect = 4;
 RadiationWatch radiationWatch(2, 3, 0, 1);
+const int sdCardPin = 4;
 File dataFile;
 char* str;
 
 void onRadiationPulse() {
+  // For each radiation update the values sent to serial and SD card in CSV.
   str = radiationWatch.csvStatus();
   Serial.println(str);
   dataFile.println(str);
+  dataFile.flush();
 }
 
 void setup()
 {
-  // Serial init.
   Serial.begin(9600);
   // SD setup.
-  if(!SD.begin(chipSelect)) {
+  if(!SD.begin(sdCardPin)) {
     Serial.println("SD card init failed, or not present");
     return;
   }
-  dataFile = SD.open("log.txt", FILE_WRITE);
+  dataFile = SD.open("radiation.csv", FILE_WRITE);
   if(!dataFile) {
     Serial.println("Failed to open file");
     return;
   }
-  // Setup the lib and register the pulse callback.
+  // Setup the lib and register the callback.
   radiationWatch.setup();
-  radiationWatch.registerRPCallback(&onRadiationPulse);
-  // Print first status on serial and on SD card.
+  radiationWatch.registerRadiationCallback(&onRadiationPulse);
+  // Print the CSV keys and the initial values.
   str = radiationWatch.csvKeys();
   Serial.println(str);
   dataFile.println(str);
@@ -42,5 +43,4 @@ void setup()
 void loop()
 {
   radiationWatch.loop();
-  dataFile.flush();
 }
