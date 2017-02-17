ed17e517779580d9b377655f9dbd125382e515b9
puuu <puuu@users.noreply.github.com>
2017-02-17 17:42:35 +0900

    SerialCsvLogger.ino: use Print class for csv
    Streaming of strings is the Arduino way and saves dynamic memory, here
    60 bytes.

--- examples/SerialCsvLogger/SerialCsvLogger.ino
+++ examples/SerialCsvLogger/SerialCsvLogger.ino
@@ -7,10 +7,39 @@ the radiation level.
 */
 
 RadiationWatch radiationWatch;
+unsigned long csvStartTime;
+
+void csvKeys(Print &print=Serial)
+{
+  // inizialise start time
+  csvStartTime = millis();
+  // Write CSV keys to a print class (default print class is Serial).
+  print.println("time(ms),count,cpm,uSv/h,uSv/hError");
+}
+
+void csvStatus(RadiationWatch radiationWatch, Print &print=Serial)
+{
+  // Write CSV to a print class (default print class is Serial).
+  // time(ms)
+  print.print(millis() - csvStartTime);
+  print.print(',');
+  // count
+  print.print(radiationWatch.radiationCount());
+  print.print(',');
+  // cpm
+  print.print(radiationWatch.cpm(), 3);
+  print.print(',');
+  // uSv/h
+  print.print(radiationWatch.uSvh(), 3);
+  print.print(',');
+  // uSv/hError
+  print.print(radiationWatch.uSvhError(), 3);
+  print.println("");
+}
 
 void onRadiationPulse() {
   // For each radiation update the values sent to serial in CSV.
-  Serial.println(radiationWatch.csvStatus());
+  csvStatus(radiationWatch);
 }
 
 void setup()
@@ -20,8 +49,8 @@ void setup()
   // Register the callback.
   radiationWatch.registerRadiationCallback(&onRadiationPulse);
   // Print the CSV keys and the initial values.
-  Serial.println(radiationWatch.csvKeys());
-  Serial.println(radiationWatch.csvStatus());
+  csvKeys();
+  csvStatus(radiationWatch);
 }
 
 void loop()
